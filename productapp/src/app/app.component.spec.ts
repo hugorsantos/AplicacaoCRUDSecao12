4be57d867e861aad14c15adf1d4eade315c0ed95
Hugo Reis Santos <hugorsantos@gmail.com>
2020-02-13 17:01:08 -0300

    Adicionando os primeiros arquivos

--- productapp/src/app/app.component.spec.ts
+++ productapp/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'productapp'`, () => {
+  it(`should have as title 'productsapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('productapp');
+    expect(app.title).toEqual('productsapp');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to productapp!');
+    expect(compiled.querySelector('.content span').textContent).toContain('productsapp app is running!');
   });
 });
